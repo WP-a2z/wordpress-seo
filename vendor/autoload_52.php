1ef0922e6cc6ab52b3ff1d1117f234fbc1a19682
Herb Miller <herb@bobbingwide.com>
2018-10-21 14:23:19 +0100

    Yoast SEO v7.8 - 2018/07/10

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit4e096bf8befa377654f558c1f5db369e::getLoader();
+return ComposerAutoloaderInit9922ad94b67f7fe50c9ff4b2c1a53952::getLoader();
