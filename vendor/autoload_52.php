7f6207334668bac8a4245f5010661295b7028385
Herb Miller <herb@bobbingwide.com>
2019-06-05 09:28:04 +0100

    wordpress-seo 11.2 - 2019-06-05

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit37210d9e4e25b6aec83ee56fdeb63ed5::getLoader();
+return ComposerAutoloaderInit1855f22676ba8649e856004038289e54::getLoader();
